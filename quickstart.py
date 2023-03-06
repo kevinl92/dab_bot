e2171993ef7941e9964577121c69e8a871f9cb06
Kevin Liou <kevinliou92@gmail.com>
2023-03-06 00:11:15 -0600

    Overhauled branching logic, separated Admin DMs, regular DMs, and server messages

--- quickstart.py
+++ quickstart.py
@@ -12,33 +12,46 @@ client = discord.Client(intents=intents)
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
-#f = open("links.txt").read().splitlines()
-
-number = 0
-
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    print('ADMIN_ID = ' + os.environ['ADMIN_ID'])
-    print('ADMIN_DM = ' + os.environ['ADMIN_DM'])
 
 @client.event
 async def on_message(message):
+    # Ignore any message that originated from this bot to prevent looping
     if message.author == client.user:
         return
+
+    # Create admin for future sending?
+    admin = await client.fetch_user(os.environ['ADMIN_ID'])
+
+    # If the message came from a DM...
+    if message.channel.type.name == 'private': 
+        # print('1')
+        print('DM from '+ message.author.name + ' : ' + message.content)
+        if message.author.id == int(os.environ['ADMIN_ID']):
+            await message.channel.send('This was a DM from the Admin.')
+
+        else:
+            await message.channel.send(message.author.name + ', you aren\'t an admin! We''ll pass your message along to Arfa.')
+            await admin.send(message.content)
+
+   # Otherwise, this came from a server...
+    elif message.channel.type.name == 'text':
+        # print('2')
+        print (message.guild.name + '/' + message.author.name + ':' + message.content)
+        # If the message starts with '$hello'
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
+
+        # If the message contains the word 'dab', case insensitive
+        if "dab" in message.content.lower():
+            await message.channel.send(random.choice(open("links.txt").read().splitlines()))
     
-    print(message.author.id)
-    print(message.channel.id)
-    print (message.author.id == int(os.environ['ADMIN_ID']))
-    print (message.channel.id == int(os.environ['ADMIN_DM']))
-
-    if message.author.id == int(os.environ['ADMIN_ID']) and message.channel.id == int(os.environ['ADMIN_DM']):
-        await message.channel.send('I can tell this was a DM.')
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if "dab" in message.content.lower():
-        await message.channel.send(random.choice(open("links.txt").read().splitlines()))
+    # Otherwise, this is from a source we don't know about
+    else:
+        # print('3')
+        print ('Unknown origin: ' + message.content)
+        return
 
 client.run(os.environ['BOT_TOKEN'], log_handler=handler, log_level=logging.DEBUG)
