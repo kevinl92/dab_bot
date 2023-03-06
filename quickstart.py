a3c7b9e0f6c60a7d1044943fba57486dbd6628fe
Kevin Liou <kevinliou92@gmail.com>
2023-03-05 21:32:07 -0600

    Added reactions to "dab"

--- quickstart.py
+++ quickstart.py
@@ -1,12 +1,15 @@
 import discord
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 @client.event
 async def on_ready():
@@ -19,5 +22,9 @@ async def on_message(message):
     
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+        #await message.reply('Hello!')
 
-client.run(os.environ['BOT_TOKEN'])
+    if "dab" in message.content.lower():
+        await message.channel.send('https://tenor.com/view/squidward-dab-100-lit-gif-15118193')
+
+client.run(os.environ['BOT_TOKEN'], log_handler=handler, log_level=logging.INFO)
